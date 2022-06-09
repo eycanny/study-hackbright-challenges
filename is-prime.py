7fb98f9d0a857e3e254929ff15cd44cd9c683478
eycanny <nancy.the.coder@gmail.com>
2022-06-08 17:09:22 -0700

    Complete function

--- is-prime.py
+++ is-prime.py
@@ -1,2 +1,15 @@
 def is_prime(num):
     """Is a number a prime number?"""
+
+    divisible_nums = set()
+
+    for i in range(1, num+1):
+        if num < 2:
+            return False
+        else:
+            if num % i == 0:
+                divisible_nums.add(i)
+        if len(divisible_nums) > 2:
+            return False
+    
+    return len(divisible_nums) <= 2
